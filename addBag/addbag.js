4489bb4f5fe884a3d24ad94ca7a63a84c409250a
shweta <shwetakodamg@gmail.com>
2022-09-14 19:11:03 +0530

    cart

--- addBag/addbag.js
+++ addBag/addbag.js
@@ -9,26 +9,48 @@ parseCart.map(item =>{
     let { category, id, price, product_link, image_link, description } = item;
 
     let div = document.createElement('div');
-    div.classList.add("cartEL")
+    div.classList.add("cartEL");
+
+     let closebtn = document.createElement("button");
+    closebtn.innerText = "x";
+    closebtn.classList.add("closebtn");
+    closebtn.id = id;
+    
 
 
     let img = document.createElement('img');
     img.src = image_link;
-    img.classList.add("imgBox")
+    img.classList.add("imgBox");
 
-    let des = document.createElement('p')
-    des.classList.add("productName")
-    des.innerHTML= description
+    let des = document.createElement('p');
+    des.classList.add("productName");
+    des.innerHTML= description;
 
-    let priceVal  = document.createElement('p')
-    priceVal.classList.add("priceName")
-    priceVal.innerText= price
+    let priceVal  = document.createElement('p');
+    priceVal.classList.add("priceName");
+    priceVal.innerText= price;
 
-    sum += Number(price)
+    sum += Number(price);
 
-    div.append(img,des, priceVal);
-    addCont.append(div)
+    div.append(closebtn,img,des, priceVal);
+    addCont.append(div);
+
+    closebtn.addEventListener("click", function (e) {
+        let productItemfromStorage = JSON.parse(localStorage.getItem("cart"));
+        console.log("working", productItemfromStorage)
+
+        productItemfromStorage.forEach((element, index )=> {
+            if(element.id == e.target.id){
+                productItemfromStorage.splice(index,1)
+            }
+            
+        });
+
+        localStorage.setItem("cart", JSON.stringify(productItemfromStorage))
+        e.target.parentElement.remove();
+    })
  
 })
+
 
 console.log(sum)
